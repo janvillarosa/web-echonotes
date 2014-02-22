ef318f0ee6e8215d563e7b973f134fdcc9a38d9b
janvillarosa <janrobertvillarosa@gmail.com>
2014-02-22 16:06:43 +0800

    Revert "Merge branch 'master' of https://github.com/janvillarosa/web-echonotes"
    This reverts commit 26aeb9ecdf033e5ff4fbddabb42d00ec4ee25700, reversing
    changes made to 34c2f73b60dc128ecf40283e11dd24eb9e2eb739.

--- app/views/note.php
+++ app/views/note.php
@@ -4,7 +4,6 @@
 <head>
 
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Note #1</title>
@@ -26,18 +25,7 @@ Page-Level Plugin CSS - Dashboard
 <link href="css/plugins/timeline/timeline.css" rel="stylesheet"> -->
 
 <!-- SB Admin CSS - Include with every page -->
-<link href="css/sb-admin.css" rel="stylesheet" />
-<link rel="stylesheet" type="text/css" href="css/demo.css" />
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300,300italic' rel='stylesheet' type='text/css' />
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="js/jplayer/jquery.jplayer.js"></script>
-<script src="js/jquery.audioslideshow.js"></script>
-<script>
-$(document).ready(function() {
-    $('.audio-slideshow').audioSlideshow();
-});
-</script>
+<link href="css/sb-admin.css" rel="stylesheet">
 </head>
 
 <body>
@@ -143,62 +131,33 @@ $(document).ready(function() {
                 </ul>
             </li>
         <!-- </nav>
-        <nav class="navbar-toolbar" role="navigation" style="margin-bottom: 0"> -->
-            <li class="divider"></li>
-            <!-- <div class="navbar-title">
-                ALGOCOM - Master's Method
-            </div>
-            <a class="navbar-play" onClick="toggleRecording(this)">
-                <i class="fa fa-play fa-fw"></i> 
-            </a> -->
-            <!-- <script>
-              // By default the plugin searches for a data-retina attribute on the image
-              $('#test-1 img').retina();
-          </script> -->
-          <div id="audio-slideshow" class="audio-slideshow" data-audio="Kurt_Vile_-_01_-_Freeway.mp3" data-audio-duration="161">
-            <div class="audio-slides">
-                <img src="http://farm2.staticflickr.com/1012/3175040364_7eef7d257f_z.jpg?zz=1" alt="[Dalen, Telemarken (i.e, Telemark), Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1012/3175040364_7eef7d257f_t.jpg" data-slide-time="0">
-                <img src="http://farm2.staticflickr.com/1145/3175012896_02056f183d_z.jpg?zz=1" alt="[Near Hjelmso, Fuglebjerget, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1145/3175012896_02056f183d_t.jpg" data-slide-time="10">
-                <img src="http://farm4.staticflickr.com/3108/3175013342_54f5b20138_z.jpg?zz=1" alt="[The Seven Sisters, Geiranger Fjord, Norway] (LOC)" data-thumbnail="http://farm4.staticflickr.com/3108/3175013342_54f5b20138_t.jpg" data-slide-time="30">
-                <img src="http://farm2.staticflickr.com/1192/3175013574_0f485bdb7f_z.jpg?zz=1" alt="[Praekestolen, Geiranger Fjord, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1192/3175013574_0f485bdb7f_t.jpg" data-slide-time="45">
-                <img src="http://farm2.staticflickr.com/1019/3174174579_0c6a63ca70_z.jpg?zz=1" alt="[Kongen og Dronningen, Bispen, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1019/3174174579_0c6a63ca70_t.jpg?" data-slide-time="70">
-                <img src="http://farm4.staticflickr.com/3117/3175014052_7484da1205_z.jpg?zz=1" alt="[Merok, Geiranger Fjord, Norway] (LOC)" data-thumbnail="http://farm4.staticflickr.com/3117/3175014052_7484da1205_t.jpg" data-slide-time="90">
-                <img src="http://farm2.staticflickr.com/1078/3175018738_03f949816c_z.jpg?zz=1" alt="[Buerbrae Glacier, Odde, Hardanger Fjord, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1078/3175018738_03f949816c_t.jpg" data-slide-time="110">
-                <img src="http://farm2.staticflickr.com/1139/3174180591_a5e318c84b_z.jpg?zz=1" alt="[Folgefond Glacier, Hardanger Fjord, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1139/3174180591_a5e318c84b_t.jpg" data-slide-time="130">
-                <img src="http://farm2.staticflickr.com/1091/3174186405_4654a14ae4_z.jpg?zz=1" alt="[Naerodalen, Hardanger Fjord, Norway] (LOC)" data-thumbnail="http://farm2.staticflickr.com/1091/3174186405_4654a14ae4_t.jpg" data-slide-time="150">
-            </div>
-            <div class="audio-control-interface">
-                <div class="play-pause-container">
-                    <a href="javascript:;" class="audio-play" tabindex="1">Play</a>
-                    <a href="javascript:;" class="audio-pause" tabindex="1">Pause</a>
+            <nav class="navbar-toolbar" role="navigation" style="margin-bottom: 0"> -->
+                <li class="divider"></li>
+                <div class="navbar-title">
+                    ALGOCOM - Master's Method
                 </div>
-                <div class="time-container">
-                    <span class="play-time"></span> / <span class="total-time"></span>
-                </div>
-                <div class="timeline">
-                    <div class="timeline-controls"></div>
-                    <div class="playhead"></div>
-                </div>
-                <div class="jplayer"></div>
-            </div>
-            <li class="divider"></li>
-        </nav>
-    </div>
+                <a class="navbar-play" onClick="toggleRecording(this)">
+                    <i class="fa fa-play fa-fw"></i>
+                </a>
+                <li class="divider"></li>
+            </nav>
+        </div>
 
 
-    <!-- Core Scripts - Include with every page -->
-    <script src="js/jquery-1.10.2.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/metisMenu/jquery.metisMenu.js"></script>
+        <!-- Core Scripts - Include with every page -->
+        <script src="js/jquery-1.10.2.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
 
-        <!-- Page-Level Plugin Scripts - Dashboard
+        <!-- Page-Level Plugin Scripts - Dashboard -->
         <script src="js/plugins/morris/raphael-2.1.0.min.js"></script>
-        <script src="js/plugins/morris/morris.js"></script> -->
+        <script src="js/plugins/morris/morris.js"></script>
 
         <!-- SB Admin Scripts - Include with every page -->
         <script src="js/sb-admin.js"></script>
 
         <!-- Page-Level Demo Scripts - Dashboard - Use for reference -->
+        <script src="js/demo/dashboard-demo.js"></script>
         <script src="js/audiodisplay.js"></script>
         <script src="js/recorderjs/recorder.js"></script>
         <script src="js/main.js"></script>
