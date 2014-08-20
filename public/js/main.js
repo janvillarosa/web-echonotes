26af94ca1db0b44a482823a71cbfe697df7cbb0b
Rhett Jason C. Buzon <rhettbuzon@gmail.com>
2014-08-21 00:22:10 +0800

    Fixed Miscellaneous Tag not working

--- public/js/main.js
+++ public/js/main.js
@@ -218,7 +218,7 @@ function uploadFile( blob ){
       tags.push("Business");
     }
     if(document.getElementById('miscellaneousTag').checked == true){
-      tags.push("Mischellaneous");
+      tags.push("Miscellaneous");
     }
 
     form.append("tCount", tags.length);
